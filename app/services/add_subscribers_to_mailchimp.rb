39b3178e1cfe56c7ca4d23354a8dcc70b74db0bb
Pavel Liser <liser.pavel@gmail.com>
2019-12-04 11:09:46 +0800

    transaction emails with SMTP and marketing emails with Gibbon gem and mailchimp

---
+++ app/services/add_subscribers_to_mailchimp.rb
@@ -0,0 +1,20 @@
+class AddSubscribersToMailchimp
+  def initialize(user)
+    @user = user
+    @gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+    @audience_id = ENV['AUDIENCE_ID']
+  end
+
+  def call
+    @gibbon.lists(@audience_id).members.create(
+      body: {
+        email_address: @user.email,
+        status: "subscribed",
+        # merge_fields: {
+        #   FNAME: @user.first_name,
+        #   LNAME: @user.last_name
+        # }
+      }
+    )
+  end
+end
